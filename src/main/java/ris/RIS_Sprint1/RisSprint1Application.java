0e04a96dd46caf4a57d2dbb8fcfcbb129f71f688
Albino-Racoon <jasa.kokalj@gmail.com>
2022-12-05 11:47:29 +0100

    testiram #RIS-2 State DONE Time 2h

--- src/main/java/ris/RIS_Sprint1/RisSprint1Application.java
+++ src/main/java/ris/RIS_Sprint1/RisSprint1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//test1
+//test11
 
 public class RisSprint1Application {
 
